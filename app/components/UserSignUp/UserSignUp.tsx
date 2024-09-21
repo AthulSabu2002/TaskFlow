7da7ffdedee216c81957d820078019907922019d
athulsabu <athulsabu75@gmail.com>
2024-09-21 19:48:50 +0530

    updated file structure

---
+++ app/components/UserSignUp/UserSignUp.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const UserSignUp:React.FC = () => {
+  return (
+    <div>UserSignUp</div>
+  )
+}
+
+export default UserSignUp
---
+++ app/signUp/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserSignUp from '../components/UserSignUp/UserSignUp'
+
+
+const SignUp:React.FC = () => {
+  return (
+    <>
+      <UserSignUp />
+    </>
+  )
+}
+
+export default SignUp
